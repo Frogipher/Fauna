0014e2a169a3fd29ef3048ce0f268d538a173705
frogipher <rodriguez.julius.rey@gmail.com>
2023-11-07 22:27:43 -0500

    Initial commit

---
+++ src/main/java/net/frogipher/fauna/FaunaClient.java
@@ -0,0 +1,10 @@
+package net.frogipher.fauna;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FaunaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
